88cab4e05e325906ec121e79573cdc18589883ae
Brad Smith <brad.alan.smith@gmail.com>
2018-07-23 07:43:50 -0700

    built out most functionality

--- config8/merger/__init__.py
+++ config8/merger/__init__.py
@@ -1,16 +1,68 @@
 
+from collections import namedtuple
 import json
 
+def _resolve_file(path):
+    with open(path) as f:
+        return json.load(f)
+
+def path_obj(path, obj):
+    if isinstance(path, str) and path.startswith('#/'):
+        path = path[2:].split('/')
+    elif isinstance(path, str):
+        raise ValueError("JSON reference path must start with '#/'")
+
+    if not isinstance(path, list):
+        raise ValueError('Path is not a list')
+
+    if len(path) == 1:
+        path = path[0]
+        if isinstance(obj, list):
+            path = int(path)
+        return obj[path]
+    else:
+        base = path.pop(0)
+        return path_obj(path, obj[base])
+
+def resolve(path, obj=None):
+    if isinstance(path, dict) and '$ref' in path:
+        path = path['$ref']
+
+    if path.startswith('#'):
+        return path_obj(path, obj)
+    elif '#' in path:
+        fpath, objpath = path.split('#', 1)
+        wholeobj = _resolve_file(fpath)
+        return path_obj('#{}'.format(objpath), wholeobj)
+    else:
+        return _resolve_file(path)
+
+def comp(target_path):
+    if isinstance(target_path, str):
+        target = resolve(target_path)
+    else:
+        target = target_path
+    parent = target.pop('@parent') if '@parent' in target else None
+    if parent is not None:
+        parent = resolve(parent)
+        return merge(parent, target)
+    return target
+
 def merge(base, overlay):
-    if isinstance(base, str):
-        base = json.loads(base)
-    if isinstance(overlay, str):
-        overlay = json.loads(overlay)
-    
+    try:
+        lock_names = set(base.pop('@lock_names'))
+    except KeyError:
+        lock_names = []
+
     for k, v in overlay.items():
-        if (k in base and isinstance(base[k], list)) and isinstance(overlay[k], list):
+        if k in lock_names:
+            print("{} is locked".format(k))
+        elif isinstance(v, dict) and '$ref' in v:
+            base[k] = resolve(v['$ref'], overlay)
+        elif isinstance(v, dict) and '@parent' in v:
+            base[k] = comp(overlay[k])
+        elif (k in base and isinstance(base[k], list)) and isinstance(overlay[k], list):
             base[k] = base[k] + overlay[k]
-            print(base[k])
         elif (k in base and isinstance(base[k], dict)) and isinstance(overlay[k], dict):
             merge(base[k], overlay[k])
         else:
--- config8/merger/test_merger.py
+++ config8/merger/test_merger.py
@@ -1,16 +1,77 @@
 
-from config8.merger import merge
+from pprint import pprint
+
+from config8.merger import merge, resolve, comp, path_obj
 
 simple = 'test_data/simple/{}.json'
 
+t_obj = {
+    'a': {
+        'b': {
+            'c': [1,2,3]
+        }
+    }
+}
+
+def test_resolve_file():
+    """Can resolve a filename."""
+    r = resolve(simple.format('overlay'))
+    assert isinstance(r, dict)
+    assert '@parent' in r
+    assert 'e' in r
+
+def test_resolve_obj():
+    """Can resolve a single-level path in an object."""
+    r = resolve('#/a', t_obj)
+    assert isinstance(r, dict)
+    assert 'b' in r
+
+def test_resolve_obj_nested_dict():
+    """Can resolve a nested jsonreference."""
+    r = resolve('#/a/b/c', t_obj)
+    assert r == [1,2,3]
+
+def test_resolve_obj_nested_list():
+    """Resolver can resolve a nested jsonreference with an array/list."""
+    r = resolve('#/a/b/c/1', t_obj)
+    assert r == 2
+
+def test_path_obj():
+    """Path evaluator can resolve a nested jsonreference with an array/list."""
+    r = path_obj('#/a/b/c/1', t_obj)
+    assert r == 2
+
+def test_compile():
+    """Compiler can compile a file from a filename."""
+    result = comp(simple.format('overlay'))
+    pprint(result)
+    _validate_overlay(result)
+    assert "@parent" not in result
+    assert result['names']['bob'] == 'old'
+    assert result['names']['jimmy'] == 'farnsworth'
+
+base = resolve(simple.format('base'))
+overlay = resolve(simple.format('overlay'))
+merge_result = merge(base, overlay)
+
 def test_merger():
-    with open(simple.format('base')) as base, \
-            open(simple.format('overlay')) as overlay:
-        base_str = base.read()
-        overlay_str = overlay.read()
-        result = merge(base_str, overlay_str)
-        assert result['a'] == 'this is an override'
-        assert result['d']["2"] == '2'
-        assert result['d']["3"] == 'three'
-        assert len(result['b']) == 4
-        assert result['b'][3] == 'item4'
+    """Merge function can deep merge two dicts."""
+    _validate_overlay(merge_result)
+    assert "@parent" in merge_result
+
+def test_merger_lock_names():
+    """Merge function respects @lock_names."""
+    assert merge_result['immortal'] == 'lives forever'
+
+def test_merger_lock_names_nested():
+    """Merge function respects nested @lock_names."""
+    assert merge_result['d']['2'] == 'two'
+
+def _validate_overlay(result):
+    """Verify the merged output of the base and overlay."""
+    assert result['a'] == 'this is an override'
+    assert result['d']["3"] == 'three'
+    assert len(result['b']) == 4
+    assert result['b'][3] == 'item4'
+    assert result['e'] == 'the first letter'
+    assert result['copy-a'] == 'this is an override'
